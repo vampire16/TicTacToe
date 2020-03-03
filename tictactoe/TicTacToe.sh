b5cc1be5652cb924cf86e0f6adc1826d4ab80169
vampire16 <siddheshsalvi525@gmai.com>
2020-03-03 09:35:49 +0530

    ADDED : Reset the board

--- tictactoe/TicTacToe.sh
+++ tictactoe/TicTacToe.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -x
 
 echo "Welcome"
+
+#CONSTANT VARIABLES
+NOOFROW=3
+NOOFCOL=3
+
+#VARIABLES
+declare -A board
+
+
+function resetBoard(){
+	for (( row=0; row<NOOFROW; row++ ))
+	do
+		for (( column=0; column<NOOFCOL; column++ ))
+		do
+			board[$row,$column]=""
+		done
+	done
+}
+resetBoard
