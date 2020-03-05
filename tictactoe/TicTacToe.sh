fc022a81813e165fd7492b6ab344724684c058e3
vampire16 <siddheshsalvi525@gmai.com>
2020-03-05 12:07:14 +0530

    ADDED : If side block is available then take the side block

--- tictactoe/TicTacToe.sh
+++ tictactoe/TicTacToe.sh
@@ -436,10 +436,33 @@ function checkCenter(){
 	fi
 }
 
+function checkSides(){
+	if [[ ${board[0,1]} == " " ]]
+	then
+		board[0,1]=$computer
+		flag4="true"
+	elif [[ ${board[1,0]} == " " ]]
+	then
+		board[1,0]=$computer
+		flag4="true"
+	elif [[ ${board[1,2]} == " " ]]
+	then
+		board[1,2]=$computer
+		flag4="true"
+	elif [[ ${board[2,1]} == " " ]]
+	then
+		board[2,1]=$computer
+		flag4="true"
+	fi
+}
+
+
 function computerTurn(){
 	flag="false"
 	flag1="false"
 	flag2="false"
+	flag3="false"
+	flag4="false"
 	count=1
    if [[ $playCount == $TOTALCOUNT ]]
    then
@@ -448,7 +471,6 @@ function computerTurn(){
    fi
 	printf "computer turn\n"
 	checkComputerWin $computer
-	#checkPlayerLoss $player
 	if [[ $flag == "true" ]]
 	then
 		getBoard
@@ -464,24 +486,28 @@ function computerTurn(){
 				checkCenter
 				if [[ $flag3 == "false" ]]
 				then
-   				pos=$((RANDOM%9 + 1))
-   				for (( i=0; i<$NOOFROW; i++ ))
-   				do
-      				for (( j=0; j<$NOOFROW; j++ ))
-      				do
-							if [[ $count == $pos ]]
-							then
-      						if [[ ${board[$i,$j]} == " " ]]
+					checkSides
+					if [[ $flag4 == "false" ]]
+					then
+   					pos=$((RANDOM%9 + 1))
+   					for (( i=0; i<$NOOFROW; i++ ))
+   					do
+      					for (( j=0; j<$NOOFROW; j++ ))
+      					do
+								if [[ $count == $pos ]]
 								then
-									board[$i,$j]=$computer
-								else
-									printf "invalid position"
-									computerTurn
+      							if [[ ${board[$i,$j]} == " " ]]
+									then
+										board[$i,$j]=$computer
+									else
+										printf "invalid position"
+										computerTurn
+									fi
 								fi
-							fi
-							((count++))
-      				done
-					done
+								((count++))
+      					done
+						done
+					fi
 				fi
 			fi
 		fi
